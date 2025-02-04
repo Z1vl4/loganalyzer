6a744bec97a146a55a07f4d9e1c2a48ac0d89bfb
A1vAnn <alva.aspnor@outlook.com>
2025-02-04 19:49:58 +0100

    added flask

--- app.py
+++ app.py
@@ -1,1 +1,16 @@
 
+from flask import Flask
+
+
+app = Flask(__name__)
+
+@app.route('/')
+
+#testa lägg in från html när den läggs in i (rätt) folder
+def index():
+    return "Hello World"
+
+
+#error printas på hemsidan atm
+if __name__ == "__main__":
+    app.run(debug=True)
