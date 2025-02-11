0cbc35869f90c9bf9a32244f3cdf1b599a97e041
Olivia Meuller <126791821+Olimeu@users.noreply.github.com>
2025-02-11 16:38:02 +0100

    Updated app.py with DB-connection

--- app.py
+++ app.py
@@ -1,16 +1,46 @@
-
-from flask import Flask
-
+from flask import Flask, render_template, request
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import CheckConstraint
 
 app = Flask(__name__)
 
-@app.route('/')
+# Anslutning till PostgreSQL
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:Examensarbete2025@localhost/loganalyzer'
 
-#testa lägg in från html när den läggs in i (rätt) folder
+db = SQLAlchemy(app)
+
+# Definierar modellen för logs-tabellen och AnalysisResults-tabellen
+class logs(db.Model):
+    __tablename__ = 'logs'
+    log_id = db.Column(db.Integer, primary_key=True)  
+    ip = db.Column(db.String(255))
+    country = db.Column(db.String(255))
+    city = db.Column(db.String(255))
+    longitude = db.Column(db.Float)
+    latitude = db.Column(db.Float)
+    device = db.Column(db.String(255))
+    os = db.Column(db.String(255))
+    browser = db.Column(db.String(255))
+    timestamp = db.Column(db.DateTime)  
+    original_source = db.Column(db.String(255))
+    log_type = db.Column(db.String(255))
+    log_message = db.Column(db.Text)
+
+class analysis_results(db.Model):
+    __tablename__ = 'analysis_results'
+    analysis_id = db.Column(db.Integer, primary_key=True)
+    log_id = db.Column(db.Integer, db.ForeignKey('logs.log_id'), nullable=False)
+    threat_level = db.Column(db.Integer, nullable=False)
+    risk_description = db.Column(db.Text, nullable=False)
+
+    __table_args__ = (
+        CheckConstraint('threat_level BETWEEN 1 AND 10', name='check_threat_level'),
+    )
+
+# Route för dashboard startsidan
+@app.route("/")
 def index():
-    return "Hello World"
+    return render_template("index.html")
 
-
-#error printas på hemsidan atm
 if __name__ == "__main__":
     app.run(debug=True)
