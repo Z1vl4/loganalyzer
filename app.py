b17bff9849f8a32c3b19fe445480eef5141fba05
Olivia Meuller <126791821+Olimeu@users.noreply.github.com>
2025-02-11 17:30:01 +0100

    Update app.py
    Updated Flask-port

--- app.py
+++ app.py
@@ -43,4 +43,5 @@ def index():
     return render_template("index.html")
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5001, debug=True)
